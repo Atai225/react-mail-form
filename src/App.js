7098f7cf581461f4f4f49516fcdaa9c9cf9f507c
Atai <atai.zhanabilov@gmail.com>
2023-07-08 00:44:30 +0600

    App is developed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import ContactForm  from "./components/ContactFrom";
+import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    fetch('/api').then((res) => res.json()).then(data => console.log(data))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="left-column">
+        <div className="content">
+          <img src="/astronaut.png" alt="austronaut" />
+        </div>
+      </div>
+      <div className="right-column">
+        <ContactForm />
+      </div>
     </div>
   );
-}
-
+};
 export default App;
